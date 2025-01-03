118a657d285d73906a44ef834d66dbe41bd4163a
zllwhu <2023202210120@whu.edu.cn>
2025-01-03 10:07:45 +0800

    202501031000

--- src/views/front/digital/Digital5.vue
+++ src/views/front/digital/Digital5.vue
@@ -1,13 +1,18 @@
 <template>
   <div>
     <el-card>
-      内容待定
+      <div style="text-align: center;">
+        <iframe
+            src="/webgl-game/index.html"
+            style="width: 100%; height: 800px; border: none;"
+        ></iframe>
+      </div>
     </el-card>
   </div>
 </template>
 
 <script setup>
-import {reactive} from "vue";
+import { reactive } from "vue";
 
-const data = reactive({})
+const data = reactive({});
 </script>
--- src/views/front/redmap/Redmap5.vue
+++ src/views/front/redmap/Redmap5.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <img style="width: 100%" :src="banner" alt="">
     <el-card style="margin-bottom: 10px">
       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;革命精神是指老一辈革命家勇于实践、勇于探索、勇于思考、奋发进取的开拓精神，不畏艰险、坚韧不拔、顽强拼搏、攻坚克难的奋斗精神和为党和人民的事业“鞠躬尽瘁、死而后已”的献身精神，是党和国家的宝贵财富。
       <br> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;中国共产党革命精神。这是新民主主义革命时期的中国共产党精神，其主题是革命，是中国共产党精神次第演进中出现的第一个主要形态。党的主要领导人对革命精神都作过重要论述，邓小平同志把革命精神概括为：“发扬革命和拼命精神，严守纪律和自我牺牲精神，大公无私和先人后己精神，压倒一切敌人、压倒一切困难的精神，坚持革命乐观主义、排除万难去争取胜利的精神。”这五种革命精神成为理解革命精神内涵的基本依据。
@@ -10,6 +11,7 @@
 
 <script setup>
 import {reactive} from "vue";
+import banner from "@/assets/img/革命精神.png";
 
 const data = reactive({
 
